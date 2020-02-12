0ae5db76fa3d6cdda4761428433ca7b68c12b681
zombie110year <zombie110year@outlook.com>
2020-02-12 14:17:09 +0800

    功能（cli）：命令行

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,37 @@
-use serde_yaml;
-use tproj::config::TprojConfigBuilder;
+#[macro_use]
+extern crate clap;
+use clap::App;
 
 fn main() {
-    let mut br = TprojConfigBuilder::new();
-    let br = br
-        .nameit("Example")
-        .auth("zombie110year")
-        .include("./main.rs")
-        .exclude("./.git/")
-        .describe("这是一个示例");
-    let obj = br.build();
-    let msg = serde_yaml::to_string(&obj).unwrap();
-    print!("{}", msg);
+    let yaml = load_yaml!("cli.yml");
+    let cli = App::from_yaml(yaml);
+    let args = cli.get_matches();
+    match args.subcommand_name() {
+        Some("apply") => {
+            let subargs = args
+                .subcommand_matches("apply")
+                .expect("未获取到 apply 子命令");
+            let aname = subargs.value_of("aname").expect("未获取到 aname 参数");
+            let dest = subargs.value_of("dest").expect("未获取到目标文件夹");
+            dbg!(aname, dest);
+        }
+        Some("create") => {
+            let subargs = args
+                .subcommand_matches("create")
+                .expect("未获取到 create 子命令 ");
+            let config = subargs.value_of("config").expect("未获取到 config 参数");
+            dbg!(config);
+        }
+        Some("list") => {
+            let subargs = args
+                .subcommand_matches("list")
+                .expect("未获取到 list 子命令");
+            let pattern = subargs.value_of("pattern").expect("未获取到 pattern 参数");
+            let verbose = subargs.is_present("verbose");
+            dbg!(pattern, verbose);
+        }
+        Some(_) => panic!("预期外的子命令"),
+        None => panic!("?"),
+    };
+    return ();
 }
